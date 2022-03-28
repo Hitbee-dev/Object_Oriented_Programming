a070f8453e74300f933b838b91cdfa57f400c98c
Hitbee-dev <ckim.esw@gmail.com>
2022-03-28 20:24:54 +0900

    수업 내용 추가

--- study/main.py
+++ study/main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+from PySide6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
+import sys
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+        # Title Name
+        self.setWindowTitle("TEST")
 
+        # Window Size조정(FixedSize넣으면 자동조절 안됨)
+        self.setFixedSize(500, 400)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+        # Button 선언 및 실행
+        self.button = QPushButton("Push!")
+        self.button.setFixedSize(100, 50)
+        self.setCentralWidget(self.button)
 
+        self.button.clicked.connect(self.button_click_event)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    def button_click_event(self):
+        print("PUSH!")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
