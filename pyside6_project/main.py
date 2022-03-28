ce656e80e46d29057a54053ebb89d5f2a759fb16
Hitbee-dev <ckim.esw@gmail.com>
2022-03-28 19:31:36 +0900

    Update: main.py

--- pyside6_project/main.py
+++ pyside6_project/main.py
@@ -6,7 +6,7 @@ from work.window import Window
 # __name__ == "__main__"을 사용하는 이유
 # main.py에 있는 모든 프로세스를 실행시키는 것이 아니라
 # 다른곳에서 참조할 때 함수만 실행할 수 있도록 하는 것
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = Window()
-    app.exec()
+# if __name__ == "__main__":
+app = QApplication(sys.argv)
+window = Window()
+app.exec()
