9e0a7dbad14c178bbc8225d4d2491bbc66fb3f06
Hitbee-dev <ckim.esw@gmail.com>
2022-03-28 19:22:06 +0900

    Update: main.py

--- pyside6_project/main.py
+++ pyside6_project/main.py
@@ -8,5 +8,5 @@ from work.window import Window
 # 다른곳에서 참조할 때 함수만 실행할 수 있도록 하는 것
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window: Window = Window()
-    sys.exit(app.exec())
+    window = Window()
+    app.exec()
